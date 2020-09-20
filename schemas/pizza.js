0725c3157f3027e89c9f1af6728a65c7a1d733d0
Juliette Rapala <jrapala@gmail.com>
2020-09-20 14:45:24 -0500

    feat: update pizza preview

--- schemas/pizza.js
+++ schemas/pizza.js
@@ -50,4 +50,22 @@ export default {
       of: [{ type: 'reference', to: [{ type: 'topping' }] }],
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      const tops = Object.values(toppings).filter(Boolean);
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
